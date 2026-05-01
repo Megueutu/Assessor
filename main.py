7caf20de2c214f98c7ced54513c4a06af4501185
meguito <doasilva2009@hotmail.com>
2026-05-01 15:37:16 -0300

    feat: main upgrade

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from app.workflow.flow import assessor_flow
 
-while 1:
-    try:
-        user_input = input("Digite sua pergunta: ").strip()
-        if user_input.lower() in ("killmyself"):
-            print("Encerrando o assistente.")
-            break
+BREAKWAYS = ('exit', 'quit', 'close', 'encerrar')
 
-        resposta = assessor_flow(pergunta_usuario=user_input, session_id="id_usuario_mas_agora_não_importa")
-        print("🤖 > " + resposta + "\n")
+def main():
+    try:
+        message = input("😎 > ").strip()
+        if message.lower() in BREAKWAYS:
+            print("\nEncerrando a conversa.")
+            return
+        
+        answer = assessor_flow(pergunta_usuario=message, session_id="id_usuario_mas_agora_não_importa")
+        print("🤖 > " + answer + "\n")
+    
     except Exception as e:
         print(f'Erro: {e}')
+
+print(f'\nAssessor iniciado! Digite: ("{'", "'.join(BREAKWAYS)}") para encerrar.')
+
+while 1:
+    main()
